be4a7ef5165d3b65234420b4cb668f9dc4bc2615
Jonathan Palardy <jonathan.palardy@gmail.com>
2008-11-03 09:34:37 -0500

    bash: (grep) and, or, nor (+ join alias)

--- bash/lib/util.sh
+++ bash/lib/util.sh
@@ -1,6 +1,19 @@
 
 #-------------------------------------------------
 
+function join() {
+  local joiner=$1; shift
+  local result=$1; shift
+
+  for x in "$@"; do
+    result+="${joiner}${x}"
+  done
+
+  echo $result
+}
+
+#-------------------------------------------------
+
 function cond_source () {
   [ -f $1 ] && . $1
 }
