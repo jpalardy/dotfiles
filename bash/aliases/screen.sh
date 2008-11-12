7b2c6cb9082649d03a83c0a392a4a6601775f45a
Jonathan Palardy <jonathan.palardy@gmail.com>
2008-11-12 09:58:58 -0500

    bash: screen_rotate alias

--- bash/aliases/screen.sh
+++ bash/aliases/screen.sh
@@ -23,3 +23,13 @@ function sc() {
   screen -r $screen || screen -x $screen
 }
 
+function screen_rotate() {
+  local session_name=${1:?"missing session name"}
+  local sleep_duration=${2:-5}
+
+  while true; do
+    screen -S $session_name -X next
+    sleep $sleep_duration
+  done
+}
+
