a6eda92ec95cf8083b13c2512c25241d4a39e06a
Jonathan Palardy <jonathan.palardy@gmail.com>
2008-12-10 16:43:00 -0500

    bash: git aliases/functions

--- bash/aliases/git.sh
+++ bash/aliases/git.sh
@@ -1,5 +1,21 @@
 
 function git_diff() {
-  git diff "$@" | vim -R -
+  git diff -w "$@" | vim -R -
+}
+
+function git_diff_head() {
+  git_diff HEAD
+}
+
+function git_diff_cached() {
+  git_diff --cached
+}
+
+function git_in() {
+  git fetch; git log master..FETCH_HEAD
+}
+
+function git_out() {
+  git fetch; git log FETCH_HEAD..master
 }
 
