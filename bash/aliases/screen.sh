2f0a865a1a772eaa9b4b02bb41b248b4bb114581
Jonathan Palardy <jonathan.palardy@gmail.com>
2008-12-27 09:06:11 -0500

    removed pick_from_list, refactored, using pick_with_vim

--- bash/aliases/screen.sh
+++ bash/aliases/screen.sh
@@ -1,26 +1,12 @@
 
 alias sls='screen -ls'
 
+function screen_r_x() {
+  screen -r $1 || screen -x $1
+}
+
 function sc() {
-  local tempfile=`mktmp` || local tempfile=/tmp/test$$
-  trap "rm -f $tempfile" 0 1 2 5 15
-
-  local screens=`screen -ls | awk '/^	/ {print $1}'`
-
-  if [ -z "$screens" ]; then
-    echo "no screens" >&2
-    return
-  fi
-
-  pick_from_list $screens 2> $tempfile
-
-  local screen=`cat_rm $tempfile`
-
-  if [ -z "$screen" ]; then
-    return 1
-  fi
-
-  screen -r $screen || screen -x $screen
+  pick_with_vim "screen -ls | awk '/^	/ {print \$1}'" "screen_r_x"
 }
 
 function screen_rotate() {
