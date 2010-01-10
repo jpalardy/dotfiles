5fe70ca6095aebd5d974f9f9ca41dd2faba1b3c1
Jonathan Palardy <jonathan.palardy@gmail.com>
2010-01-10 11:44:58 -0500

    bash: capistrano task completion

---
+++ bash/completion/cap.sh
@@ -0,0 +1,19 @@
+
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+function _check_capfile() {
+  if [ ! -e Capfile ]; then
+    return
+  fi
+
+  local cache_file=".cache_cap_t"
+
+  if [ ! -e "$cache_file" ]; then
+    cap -T | awk '/^cap / {print $2}' > $cache_file
+  fi
+
+  local tasks=$(cat $cache_file)
+  COMPREPLY=( $(compgen -W "${tasks}" -- $2) )
+}
+complete -F _check_capfile -o default cap
+
