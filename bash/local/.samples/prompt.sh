c4f681a9f27b0f47e0b409718ae4a4f0e4eb158e
Jonathan Palardy <jonathan.palardy@gmail.com>
2010-01-10 01:09:01 -0500

    bash: bash/local sample files

---
+++ bash/local/.samples/prompt.sh
@@ -0,0 +1,4 @@
+
+PS1_COLOR=$(tput setaf 166)  # orange
+PATH_COLOR=$(tput setaf 236) # gray
+
---
+++ bash/local/.samples/variables.sh
@@ -0,0 +1,9 @@
+
+export DB_HOST="localhost"
+export DB_ADAPTER="sqlite"
+export DB_NAME="db/development.sqlite3"
+export DB_USER=""
+
+export AMAZON_ACCESS_KEY_ID=""
+export AMAZON_SECRET_ACCESS_KEY=""
+
