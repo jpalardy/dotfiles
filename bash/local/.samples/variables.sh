29b9031f6a4a052edc8269b0c60313b1d2d19af8
Jonathan Palardy <jonathan.palardy@gmail.com>
2010-09-17 20:31:33 -0400

    bash: better sample variables file

--- bash/local/.samples/variables.sh
+++ bash/local/.samples/variables.sh
@@ -1,7 +1,12 @@
 
-export DB_HOST="localhost"
+# sqlite3
 export DB_ADAPTER="sqlite"
 export DB_NAME="db/development.sqlite3"
+
+# mysql
+export DB_ADAPTER="mysql"
+export DB_HOST="localhost"
+export DB_NAME=""
 export DB_USER=""
 
 export AMAZON_ACCESS_KEY_ID=""
