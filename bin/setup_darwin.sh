20e9445e615e49cb1ab14857af26190589e6c6ae
Jonathan Palardy <jonathan.palardy@gmail.com>
2010-10-21 21:26:08 -0400

    ad hoc, informally-specified, bug-ridden, slow implementation of half of chef
    :-D

--- bin/setup_darwin.sh
+++ bin/setup_darwin.sh
@@ -15,25 +15,37 @@ ensure_directory() {
   fi
 }
 
-# ensure_and_setup(original_command, command)
-ensure_and_setup() {
+# safe_ln(src, dst)
+safe_ln() {
+  if [ -L $2 ]; then
+    return
+  fi
+
+  ln -vs $1 $2
+}
+
+# ensure_and_link(original_command, command)
+ensure_and_link() {
   ensure_command $1
-  ln -v -s $(which $1) $HOME/local/bin/$2
+
+  safe_ln $(which $1) $HOME/local/bin/$2
 
   local old_man_path=$(man -w $1)
   local new_man_path=$(echo $old_man_path | sed s/$1/$2/ | sed s_.*share/man_$HOME/local/share/man_)
-  ln -v -s $old_man_path $new_man_path
+
+  safe_ln $old_man_path $new_man_path
 }
 
 #-------------------------------------------------
 
 ensure_directory "$HOME/local/bin"
-ensure_directory "$HOME/local/share/man"
+ensure_directory "$HOME/local/share/man/man1"
 
-ensure_and_setup gls   ls
-ensure_and_setup gtac  tac
-ensure_and_setup gfind find
-ensure_and_setup gdu   du
-ensure_and_setup gsed  sed
-ensure_and_setup gseq  seq
+ensure_and_link gls   ls
+ensure_and_link gtac  tac
+ensure_and_link gfind find
+ensure_and_link gdu   du
+ensure_and_link gsed  sed
+ensure_and_link gseq  seq
+ensure_and_link gawk  awk
 
