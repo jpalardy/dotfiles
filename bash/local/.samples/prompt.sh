98401044f53c0aab9d406dab2bf110e67bcff881
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-04-15 22:25:12 -0400

    bash: collect previously used PS1 colors

--- bash/local/.samples/prompt.sh
+++ bash/local/.samples/prompt.sh
@@ -1,6 +1,7 @@
 
-PS1_COLOR=$(tput setaf 166)  # orange
 # PS1_COLOR=$(tput setaf 28)  # green
+# PS1_COLOR=$(tput setaf 161)  # hot pink
+PS1_COLOR=$(tput setaf 166)  # orange
 
 PATH_COLOR=$(tput setaf 236) # gray
 
