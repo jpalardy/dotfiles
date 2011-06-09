9e40a46259f0687f01f5920dda606c9bc39e3178
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-06-09 14:27:45 -0400

    bash: use tput for escape sequences

--- bash/commands/term.sh
+++ bash/commands/term.sh
@@ -2,31 +2,23 @@
 term_colors() {
   local fg bg
 
-  for fg in 30 31 32 33 34 35 36 37; do
-    for bg in 40 41 42 43 44 45 46 47; do
-      echo -n -e "\e[${fg};${bg}m (${fg},${bg}) \e[0m"
-    done
-    echo
-    for bg in 40 41 42 43 44 45 46 47; do
-      echo -n -e "\e[1;${fg};${bg}m (${fg},${bg}) \e[0m"
+  for fg in 0 1 2 3 4 5 6 7; do
+    for bg in 0 1 2 3 4 5 6 7; do
+      fg_bg $fg $bg
     done
     echo
   done
+}
 
-  echo
+fg_bg() {
+  local fg=$1
+  local bg=${2:-0}
+  printf "%3d,%3d  " $fg $bg
+  echo "$(tput setaf $fg)$(tput setab $bg) The quick brown fox jumps over the lazy dog $(tput sgr0)"
+}
 
-  for fg in 30 31 32 33 34 35 36 37; do
-    for bg in 40 41 42 43 44 45 46 47; do
-      echo -n -e "\e[5m"
-      echo -n -e "\e[${fg};${bg}m (${fg},${bg}) \e[0m"
-    done
-    echo
-    for bg in 40 41 42 43 44 45 46 47; do
-      echo -n -e "\e[5m"
-      echo -n -e "\e[1;${fg};${bg}m (${fg},${bg}) \e[0m"
-    done
-    echo
-  done
+fg_bg_random() {
+  fg_bg $(shuf -n 1 -i 0-255) $(shuf -n 1 -i 0-255)
 }
 
 title() {
