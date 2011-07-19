0df7f8d449693a8370e17ac6a7efb0165012fc9d
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-07-19 10:59:15 -0400

    bash: explicit completion for bake
    Preparing for a 'bake' package.

--- bash/completion/bake.sh
+++ bash/completion/bake.sh
@@ -1,6 +1,16 @@
 
 _check_bakefile() {
-  cache_complete Bakefile .cache_bake "bake | awk '/^bake / {print \$2}'"
+  local src_file="Bakefile"
+  local generate_cmd="bake | awk '/^bake / {print \$2}'"
+  local word=${COMP_WORDS[COMP_CWORD]}
+
+  if [ ! -e "$src_file" ]; then
+    return
+  fi
+
+  COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+  local tasks=$(eval $generate_cmd)
+  COMPREPLY=( $(compgen -W "${tasks}" -- "$word") )
 }
 complete -F _check_bakefile -o default bake
 
