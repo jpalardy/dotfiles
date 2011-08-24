ef3e4777bdb8e71f9067affe72aabcd891e8314a
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-08-24 16:59:17 -0400

    bash: unused scripts

--- bin/print_yaml.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'yaml'
-require 'pp'
-
-pp YAML::load_file(ARGV[0])
-
--- bin/textilize.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'rubygems'
-require 'redcloth'
-
-puts RedCloth.new(ARGF.read).to_html.gsub(/\t/, '  ')
-
