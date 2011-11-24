abde2b7dad5ae99e24af8fe5c93e860f2872c963
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-11-24 00:21:26 -0500

    bash: don't emulate RVM, project environments will be loaded on demand

--- bash/commands/rvm.sh
+++ bash/commands/rvm.sh
@@ -1,15 +1,7 @@
 
 if [ -d "$HOME/.rvm" ]; then
-  rvm_reload_flag=1
   source $HOME/.rvm/scripts/rvm
   source $HOME/.rvm/scripts/completion
-
-  cd() {
-    better_cd "$@"
-    result=$?
-    __rvm_project_rvmrc
-    return $result
-  }
 
   alias rgl="rvm gemset list"
 fi
