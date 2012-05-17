699814617fc0818e61860e05299a34ede83d2809
Jonathan Palardy <jonathan.palardy@gmail.com>
2012-05-17 11:18:36 -0400

    bash: Makefile targets completion

--- bash/completion/make.bash
+++ bash/completion/make.bash
@@ -1,6 +1,6 @@
 
 _check_makefile() {
-  cache_complete Makefile .cache_make "echo"
+  cache_complete Makefile .cache_make "make -rpn | sed -n -e '/^$/ { n ; /^[^ ]*:/p }' | sed -e 's/://g'"
 }
 complete -F _check_makefile -o default make
 
