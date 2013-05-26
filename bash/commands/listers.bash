252c143278ee86149d11f90f80c1395ce89c0a8a
Jonathan Palardy <jonathan.palardy@gmail.com>
2013-05-26 15:27:03 -0400

    bash: generate numbered aliases

--- bash/commands/listers.bash
+++ bash/commands/listers.bash
@@ -18,15 +18,22 @@ lister() {
 "
 }
 
+lister_num() {
+  local name=$1
+  local n=$2
+
+  for i in $(seq $2); do
+    alias ${name}${i}="$name $i"
+  done
+}
+
 #-------------------------------------------------
 
 lister "g"    "cd"
 lister "qc"   "eval"
 lister "rg"   "rvm use"
 lister "warp" "ssh"     "awk '{print \\\$1}'"
-lister "rs"   "eval"
 
-for i in $(seq 20); do
-  alias g$i="g $i"
-done
+lister_num g 12
+lister_num rg 5
 
