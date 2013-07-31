32bdfec844375a30c12a26b5c3ef640a30c5e1fa
Jonathan Palardy <jonathan.palardy@gmail.com>
2013-07-31 15:26:33 -0700

    bash: improve git_review

--- bash/commands/git.bash
+++ bash/commands/git.bash
@@ -5,8 +5,21 @@ git_diff() {
   git diff -w "$@" | vim -R -
 }
 
+dot_wait() {
+  local i=0
+  for i in $(seq 1 ${1:-5}); do
+    read -t 1 -p '.'
+    if [ $? -eq 0 ]; then
+      return
+    fi
+  done
+  echo
+}
+
 git_review() {
   local commits=$(git log --format="%H" "$@" | tac)
+  git log --oneline "$@"
+  dot_wait 3
   for commit in $commits; do
     git show $commit | vim -R -
   done
