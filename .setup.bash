5088566b5c7a42d08cf0249c95661e7ff6662104
Jonathan Palardy <jonathan.palardy@gmail.com>
2015-10-03 02:17:34 -0700

    jshint --> eslint

--- .setup.bash
+++ .setup.bash
@@ -31,7 +31,7 @@ relink .rvmrc        $DOTFILES/rvmrc
 relink .gemrc        $DOTFILES/gemrc
 
 relink .npmrc        $DOTFILES/npmrc
-relink .jshintrc     $DOTFILES/jshintrc
+relink .eslintrc     $DOTFILES/eslintrc
 
 relink .Rprofile     $DOTFILES/r/Rprofile
 
--- vim/ftplugin/javascript.vim
+++ vim/ftplugin/javascript.vim
@@ -2,3 +2,5 @@
 " require('x') --> var x = require('x') -- make var out of require
 TabMacro '^require',    "^wwyiw^ivar \<esc>pa = "
 
+let g:syntastic_javascript_checkers = ['eslint']
+
