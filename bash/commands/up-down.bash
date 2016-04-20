741339d2fd1d8ad49fc7632c9b6f29bf6b45565f
Jonathan Palardy <jonathan.palardy@gmail.com>
2016-04-19 21:32:10 -0700

    bash: up-down

---
+++ bash/commands/up-down.bash
@@ -0,0 +1,17 @@
+
+up() {
+  rsync -avz --progress "$@" jpalardy.com:up-down/
+}
+
+down() {
+  if [ "$1" == "-d" ]; then
+    ssh jpalardy.com "cd up-down && rm -v *"
+    return
+  fi
+  if [ -z "$@" ]; then
+    ssh jpalardy.com "cd up-down && ls -1"
+    return
+  fi
+  rsync -avz --progress jpalardy.com:up-down/"$@" .
+}
+
