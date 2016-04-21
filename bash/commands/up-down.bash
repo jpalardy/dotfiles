bee6e6f4b8b2b3d34a25741f376e2f6c798719d8
Jonathan Palardy <jonathan.palardy@gmail.com>
2016-04-20 20:37:52 -0700

    bash: up-down, symmetry for empty and -d

--- bash/commands/up-down.bash
+++ bash/commands/up-down.bash
@@ -1,5 +1,13 @@
 
 up() {
+  if [ "$1" == "-d" ]; then
+    ssh jpalardy.com "cd up-down && rm -v *"
+    return
+  fi
+  if [ -z "$@" ]; then
+    ssh jpalardy.com "cd up-down && ls -1"
+    return
+  fi
   rsync -avz --progress "$@" jpalardy.com:up-down/
 }
 
