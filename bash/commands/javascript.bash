0bbc78e2892382b9e107315425b0b75bc98a6ed0
Jonathan Palardy <jonathan.palardy@gmail.com>
2016-07-29 16:56:38 -0700

    javascript changes

--- bash/commands/javascript.bash
+++ bash/commands/javascript.bash
@@ -1,5 +1,5 @@
 
 npm_list() {
-  npm list --json | jq '.dependencies | to_entries[] | "\(.key) \(.value.version)"' -r | column -t
+  npm list --json "$@" | jq '.dependencies | to_entries[] | "\(.key) \(.value.version)"' -r | column -t
 }
 
