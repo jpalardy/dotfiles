8481b8617ab5fe2ac3c9e9cc0627f9c6de2ee5c3
Jonathan Palardy <jonathan.palardy@gmail.com>
2016-12-16 15:06:56 -0500

    git: use --date-order for glg

--- bash/commands/git.bash
+++ bash/commands/git.bash
@@ -36,8 +36,7 @@ git_review() {
 
 #-------------------------------------------------
 
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset%C(yellow)%d%Creset %C(dim white)%an%Creset' --abbrev-commit --date=relative --topo-order --all"
-alias glgdo="glg --date-order"
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset%C(yellow)%d%Creset %C(dim white)%an%Creset' --abbrev-commit --date=relative --date-order --all"
 
 #-------------------------------------------------
 
