c58bcbe0232fd48f165a7b7ce5b837f3997ef3ee
Jonathan Palardy <jonathan.palardy@gmail.com>
2018-05-29 14:49:16 -0700

    Use new droplet address

--- bash/commands/up-down.bash
+++ bash/commands/up-down.bash
@@ -1,33 +1,35 @@
 
 up() {
+  local UP_DOWN_HOST="droplet.jpalardy.com"
   if [ "$1" == "-a" ]; then
-    rsync -avz --progress * jpalardy.com:up-down/
+    rsync -avz --progress ./* $UP_DOWN_HOST:up-down/
     return
   fi
   if [ "$1" == "-d" ]; then
-    ssh jpalardy.com "cd up-down && rm -rfv *"
+    ssh $UP_DOWN_HOST "cd up-down && rm -rfv *"
     return
   fi
   if [ -z "$1" ]; then
-    ssh jpalardy.com "cd up-down && ls -1"
+    ssh $UP_DOWN_HOST "cd up-down && ls -1"
     return
   fi
-  rsync -avz --progress "$@" jpalardy.com:up-down/
+  rsync -avz --progress "$@" $UP_DOWN_HOST:up-down/
 }
 
 down() {
+  local UP_DOWN_HOST="droplet.jpalardy.com"
   if [ "$1" == "-a" ]; then
-    rsync -avz --progress jpalardy.com:up-down/* .
+    rsync -avz --progress $UP_DOWN_HOST:up-down/* .
     return
   fi
   if [ "$1" == "-d" ]; then
-    ssh jpalardy.com "cd up-down && rm -rfv *"
+    ssh $UP_DOWN_HOST "cd up-down && rm -rfv *"
     return
   fi
   if [ -z "$1" ]; then
-    ssh jpalardy.com "cd up-down && ls -1"
+    ssh $UP_DOWN_HOST "cd up-down && ls -1"
     return
   fi
-  rsync -avz --progress jpalardy.com:up-down/"$@" .
+  rsync -avz --progress "$UP_DOWN_HOST:up-down/$1" .
 }
 
