a8ab0c62ec91beba0d846f9ecec15c2384d06f45
Jonathan Palardy <jonathan.palardy@gmail.com>
2018-09-28 09:15:23 -0700

    Improve bash primer

--- bin/primers/bash/untitled.bash
+++ bin/primers/bash/untitled.bash
@@ -9,3 +9,52 @@ set -o pipefail # ...
 # remove trap
 # trap - INT TERM EXIT
 
+#-------------------------------------------------
+
+usage() {
+  cat >&2 <<EOM
+Usage: $(basename "$0") [OPTION]...
+
+  -a VALUE    argument description
+              line two
+              line three
+  -b VALUE    argument description
+  -c          switch description
+              line two
+  -d          switch description
+              line two
+              line three
+              line four
+  -h          display help
+EOM
+  exit 1
+}
+
+# init switch flags
+c=0
+d=0
+
+while getopts "a:b:cdh" opt; do
+  case "$opt" in
+    a)
+      a=$OPTARG
+      ;;
+    b)
+      b=$OPTARG
+      ;;
+    c)
+      c=1
+      ;;
+    d)
+      d=1
+      ;;
+    h|*)
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+#-------------------------------------------------
+
