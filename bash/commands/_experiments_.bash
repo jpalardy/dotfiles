0541eb9522d78aa4a608294c37b60c744922ce5d
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-01-24 23:03:00 -0800

    Add experiment: implode

--- bash/commands/_experiments_.bash
+++ bash/commands/_experiments_.bash
@@ -49,3 +49,16 @@ HOME() {
   rg -N "${@:-" "}" ~/.HOME
 }
 
+# 2019-01-24
+implode() {
+  local target="$PWD"
+  local code
+  code=$(seq 0 9 | shuf -r -n 4 | awk '{printf "%s", $0}')
+  read -r -p "$code: delete $target ? " input
+  if [ "$input" != "$code" ]; then
+    return
+  fi
+  cd ..
+  rm -rf "$target"
+}
+
