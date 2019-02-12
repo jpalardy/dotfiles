6e47b6c98a0401cff375fae064da75133ef42295
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-02-11 20:30:11 -0800

    Improve bash_completion loading

--- bash/completion/bash.bash
+++ bash/completion/bash.bash
@@ -1,14 +1,16 @@
 
-for dir in /usr/local/etc /etc; do
-  if [ -e ${dir}/bash_completion ]; then
-    source ${dir}/bash_completion
-    complete -r vim
-    complete -r sqlite3
-    break
-  fi
-done
-
-if [ -z "$BASH_COMPLETION" ]; then
+load_bash_completion() {
+  for dir in /usr/local/etc /etc; do
+    if [ -e ${dir}/bash_completion ]; then
+      source ${dir}/bash_completion
+      complete -r vim
+      complete -r sqlite3 2>/dev/null
+      return
+    fi
+  done
   echo "no bash-completion"
-fi
+}
+
+load_bash_completion
+unset load_bash_completion
 
