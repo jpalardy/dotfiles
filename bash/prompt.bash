23352ac0c5d0430ebbf7017199b8269e55b934b8
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-03-05 15:14:35 -0800

    Show exit code when != 0

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -5,7 +5,16 @@ else
   export TERM="xterm-256color"
 fi
 
+COLOR_RED=$(tput setaf 1)
 ATTR_RESET=$(tput sgr0)
+
+export PROMPT_COMMAND=set_prompt
+set_prompt() {
+  local exit=$?
+  if [ "$exit" -ne 0 ]; then
+    echo -e "${COLOR_RED}exit: ${exit}${ATTR_RESET}"
+  fi
+}
 
 PS1='\[${PS1_COLOR}\]\h\[${ATTR_RESET}\] '
 
