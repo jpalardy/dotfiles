8616357930137caeb54b06c632a4dffc79b68690
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-04-09 17:27:12 -0700

    Remove warning if vim completion isn't available

--- bash/completion/bash.bash
+++ bash/completion/bash.bash
@@ -3,7 +3,7 @@ load_bash_completion() {
   for dir in /usr/local/etc /etc; do
     if [ -e ${dir}/bash_completion ]; then
       source ${dir}/bash_completion
-      complete -r vim
+      complete -r vim 2>/dev/null
       complete -r sqlite3 2>/dev/null
       return
     fi
