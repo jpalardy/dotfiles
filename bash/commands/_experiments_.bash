59c6c07316e15716f6265de02514686a8177ec63
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-04-13 16:42:28 -0700

    Remove unused experiments

--- bash/commands/_experiments_.bash
+++ bash/commands/_experiments_.bash
@@ -1,9 +1,4 @@
 
-npm2svg() {
-  npm ls --json "$@" | npx npm2dot | grep -v "undefined" | twopi -Tsvg -o out.svg -Goverlap=false
-  open -a "Google Chrome" out.svg
-}
-
 title() {
   echo -ne "\033]0;"$*"\007"
 }
@@ -18,18 +13,6 @@ waru() {
 
 inet() {
   ifconfig | awk '/^[^\t]/ {interface=$1} $1 == "inet" {address=$2; print interface, address}' | column -t
-}
-
-minvim() {
-  cat <<END
-set hlsearch
-set incsearch
-set ignorecase
-END
-}
-
-step() {
-  mkdir "$@" && cd "$@"
 }
 
 glss() {
