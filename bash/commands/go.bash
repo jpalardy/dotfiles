3301e2978700ede2d81db46de780fa899eb50602
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-04-22 10:19:16 -0700

    Add go experiments

--- bash/commands/go.bash
+++ bash/commands/go.bash
@@ -4,3 +4,16 @@ gopath() {
   export PATH="$PWD/bin:$PATH"
 }
 
+go-where-main() {
+  rg 'package main' -l
+}
+
+go-types() {
+  rg '^type \w+' | awk '{print $3}' | sort | uniq -c | sort -nr
+}
+
+go-project-summary() {
+  echo "files:" $(fne go | wc -l) "($(fne go | grep _test.go$ | wc -l) tests)"
+  echo "lines:" $(fne go | xargs cat | wc -l)
+}
+
