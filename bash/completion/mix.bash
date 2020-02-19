56ce0295673189b40ae0b44b867fb432e4a8773f
Jonathan Palardy <jonathan.palardy@gmail.com>
2020-02-18 20:07:04 -0800

    Simplify mix completion #bash

--- bash/completion/mix.bash
+++ bash/completion/mix.bash
@@ -1,19 +1,12 @@
 
-# https://github.com/ai/rake-completion/blob/master/rake
 _complete_mix_command() {
-  [ -f mix.exs ] || exit 0
-  # blow out cache if stale
-  local recent
-  recent=$(ls -t .mix-tasks mix.exs 2>/dev/null | head -n 1)
-  if [ "$recent" != ".mix-tasks" ]; then
-    rm -f .mix-tasks
+  if [ -z "$_mix_task_list" ]; then
+    _mix_task_list="$(mix help --search "" | awk '{print $2}')"
   fi
-  # regen cache if missing
-  if [ ! -f ".mix-tasks" ]; then
-    mix help --search "" | awk '{print $2}' > .mix-tasks
-  fi
-  awk -v re="^$2" '$0 ~ re' .mix-tasks
-  return $?
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$_mix_task_list" "$cur") )
+  return 0
 }
 
-complete -C _complete_mix_command -o default mix
+complete -F _complete_mix_command -o default mix
+
