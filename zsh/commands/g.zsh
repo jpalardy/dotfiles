14990f05ee0735f685b0e1a02e61c13e53f7e166
Jonathan Palardy <jonathan.palardy@gmail.com>
2020-07-07 20:09:13 -0700

    Add -a/-e handling inside `g` #zsh

--- zsh/commands/g.zsh
+++ zsh/commands/g.zsh
@@ -1,5 +1,22 @@
 
 g() {
+  local config="$HOME/.lists/g"
+  if [ "$1" = "-e" ]; then
+    $EDITOR $config
+    return
+  fi
+  if [ "$1" = "-a" ]; then
+    shift
+    local key=${1:-${PWD:t}}
+    local value=${2:-$PWD}
+    value=${value#$HOME/}
+    if [ $# -lt 2 ]; then
+      echo 2>&1 "$key = $value"
+    fi
+    echo "$key = $value" >> $config
+    return
+  fi
+  #...
   local dst
   dst=$(awk -v "key=$1" -F' += +' '
     { LUT[$1] = $2 }
@@ -12,9 +29,9 @@ g() {
       }
       print LUT[key]
     }
-  ' "$HOME/.lists/g")
+  ' $config)
   if [ -z "$dst" ]; then
-    cat "$HOME/.lists/g"
+    cat $config
     return
   fi
   cd "$dst"
@@ -24,14 +41,4 @@ _complete_g() {
   compadd $(awk -F' += +' '{ print $1 }' "$HOME/.lists/g")
 }
 compdef _complete_g g
-
-g.add() {
-  local key=${1:-$(basename "$PWD")}
-  local value=${2:-$PWD}
-  value=${value#$HOME/}
-  if [ $# -lt 2 ]; then
-    echo 2>&1 "$key = $value"
-  fi
-  echo "$key = $value" >> "$HOME/.lists/g"
-}
 
