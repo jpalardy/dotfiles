9c5dc8996b32d95a884fda0a563741a8eec1141e
Jonathan Palardy <jonathan.palardy@gmail.com>
2020-08-29 13:28:15 -0700

    Add __experiments.zsh for new things #zsh

---
+++ zsh/commands/__experiments.zsh
@@ -0,0 +1,19 @@
+
+# 2019-01-24
+implode() {
+  local target="$PWD"
+  local code
+  code=$(seq 0 9 | shuf -r -n 4 | awk '{printf "%s", $0}')
+  echo -n "$code: delete $target ? "
+  read input
+  if [ "$input" != "$code" ]; then
+    return
+  fi
+  cd ..
+  rm -rf "$target"
+}
+
+# 2020-08-29
+alias -g _V='| vim -R -'
+alias -g _G='| rg'
+
