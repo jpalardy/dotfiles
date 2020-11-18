344514c52350274aa3afdc941a92404c0de2bc8e
Jonathan Palardy <jonathan.palardy@gmail.com>
2020-11-18 14:54:17 -0800

    Push git-switch-master-to-main cheat sheet #zsh

--- zsh/commands/__experiments.zsh
+++ zsh/commands/__experiments.zsh
@@ -30,3 +30,17 @@ alias tmrw='tmux rename-window $PWD:t'
 
 # 2020-11-12
 alias rsy="rsync -av --progress"
+
+# 2020-11-18
+git-switch-master-to-main() {
+  cat <<END
+1. git branch -m master main
+2. git push -u origin main
+3. git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main
+4. remote:
+   a) github: switch default branch
+   b) bare repo: git symbolic-ref HEAD refs/heads/main
+5. git push origin --delete master
+END
+}
+
