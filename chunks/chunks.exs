c201cb9ec3f1739617d993a9f78e2731cd9d1df5
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-01-02 12:01:56 -0800

    Add .iex.exs #chunks

---
+++ chunks/chunks.exs
@@ -0,0 +1,8 @@
+#-------------------------------------------------
+# .iex.exs                                                                                                                                 ~/projects/2020-11-07-bp-port-to-elixir/bookpiles
+#-------------------------------------------------
+
+require Ecto.Query
+
+alias Bookpiles.Repo
+
