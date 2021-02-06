5893f2b0f2eb21e39a74fee50a1ba4d0150312cb
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-02-05 17:37:52 -0800

    Remove `cdf`, as an experiment #zsh

--- zsh/commands/cd.zsh
+++ zsh/commands/cd.zsh
@@ -44,31 +44,6 @@ cdl() {
   run-not-blank cd $(ls | fzf -q "$1")
 }
 
-cdf() {
-  local vim_after=0
-  local ff_flags=""
-  while true; do
-    if [ "$1" = "-u" ]; then   # -u belongs to ff
-      ff_flags="-u"
-      shift
-      continue
-    fi
-    if [ "$1" = "-v" ]; then   # -v belongs to "vim"
-      vim_after=1
-      shift
-      continue
-    fi
-    break
-  done
-  local target
-  target=$(ff $ff_flags | fzf -q "$1")
-  run-not-blank cd "$target"
-  if [ $vim_after = 1 ]; then
-    run-not-blank vim "${target:t}"
-  fi
-}
-alias cdfv="cdf -v"
-
 run-not-blank() {
   if [ $# -lt 2 ]; then
     return
