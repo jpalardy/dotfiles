6522307e8b4c600ba6a8e96fdbb30b647ef8c155
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-02-14 20:41:45 -0800

    Remove unused `implode` experiment #zsh

--- zsh/commands/__experiments.zsh
+++ zsh/commands/__experiments.zsh
@@ -1,18 +1,4 @@
 
-# 2019-01-24
-implode() {
-  local target="$PWD"
-  local code
-  code=$(seq 0 9 | shuf -r -n 4 | awk '{printf "%s", $0}')
-  echo -n "$code: delete $target ? "
-  read input
-  if [ "$input" != "$code" ]; then
-    return
-  fi
-  cd ..
-  rm -rf "$target"
-}
-
 # 2021-01-28
 # reset -- if sourced multiple times
 unalias tcd 2>/dev/null
