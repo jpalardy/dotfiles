846405189ee043eb25d8b15fb7b0cbefaf6443f6
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-02-16 10:08:38 -0800

    Add GenServer-related helpers #vim

--- chunks/chunks.exs
+++ chunks/chunks.exs
@@ -6,3 +6,21 @@ require Ecto.Query
 
 alias Bookpiles.Repo
 
+#-------------------------------------------------
+# GenServer
+#-------------------------------------------------
+
+defmodule Some.Server do
+  use GenServer
+
+  def init(initial_number) do
+    {:ok, initial_number}
+  end
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number + 1}
+  end
+end
+
+#-------------------------------------------------
+
