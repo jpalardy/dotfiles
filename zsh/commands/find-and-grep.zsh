ae017251b5ce830a239e25726d61b1db7c1cab52
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-03-07 18:59:41 -0800

    Move export with others #zsh

--- zsh/commands/find-and-grep.zsh
+++ zsh/commands/find-and-grep.zsh
@@ -27,9 +27,3 @@ find-empty() {
   find . -type "${1:-f}" -empty
 }
 
-#-------------------------------------------------
-# fzf
-#-------------------------------------------------
-
-export FZF_DEFAULT_OPTS='--reverse --info=hidden --height=20 --cycle --border --multi'
-
