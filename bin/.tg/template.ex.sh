f94588d7a6da612b7a17fd69b35f2585679b45d1
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-04-01 10:58:06 -0700

    Fix and simplify elixir template #shell

--- bin/.tg/template.ex.sh
+++ bin/.tg/template.ex.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
+# lib/text_client/prompter.ex -> TextClient.Prompter
+# - remove leading "lib/", trailing ".ex"
+# - upcase first letter, and letters after _ or /
+# - remove _
+# - replace / with .
+
 module_name() {
-  echo "$1" | sed -e 's_^lib\/__' -e 's/\.ex$//' | awk '
-    function join(array, sep) {
-      result = array[1]
-      for (i = 2; i <= length(array); i++) {
-        result = result sep array[i]
-      }
-      return result
-    }
-
-    function capitalize(word) {
-      return toupper(substr(word, 1, 1)) tolower(substr(word, 2))
-    }
-
-    {
-      split($0, parts, "/")
-      for (i = 1; i <= length(parts); i++) {
-        parts[i] = capitalize(parts[i])
-      }
-      print join(parts, ".")
-    }
-  '
+  echo "$1" | sed -E -e 's#(^lib/|\.ex$)##g' -e 's#((^|[_/]).)#\U\1#g' -e 's#_##g' -e 's#/#.#g'
 }
 
 cat <<EOF
