9559a80f59c21f96fbf3a3e77ca7fb76e7241577
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-04-18 13:29:43 -0700

    Add exercism helpers, to be removed later #zsh

--- zsh/commands/elixir.zsh
+++ zsh/commands/elixir.zsh
@@ -4,3 +4,13 @@ alias im="iex -S mix"
 
 alias mtff="mix test --seed 0 --max-failures 1"
 
+#-------------------------------------------------
+# exercism helpers...
+#-------------------------------------------------
+
+alias vu='vim $(ff | rg ex | rg -v -e test_helper -e mix.exs) -O'
+
+fix-pending() {
+  sed -E -e 's/^( +)@tag :pending/\1# @tag :pending/' --in-place $(find test -name '*_test.exs')
+}
+
