9868d52f5da4bd282da7ec0000c28526bee98333
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-05-18 11:40:44 -0700

    Extend g command, with argparse #fish

--- config/fish/conf.d/g.fish
+++ config/fish/conf.d/g.fish
@@ -1,6 +1,29 @@
-
 function g
   set -l config "$HOME/.lists/g"
+
+  argparse --name g add edit -- $argv
+  or return
+
+  # --add
+  if set -q _flag_add
+    set -l key (basename $PWD)
+    set -l value (pwd | sed -e "s#"$HOME"/##")
+    echo "$key = $value" | tee -a $config
+    return
+  end
+
+  # --edit
+  if set -q _flag_edit
+    $EDITOR $config
+    return
+  end
+
+  # empty...
+  if test -z "$argv"
+    cat $config
+    return
+  end
+
   awk -v "key=$argv[1]" -F' += +' '
     $1 == key {
       dst = $2
@@ -18,5 +41,5 @@ function __g_keys
   awk -F' += +' '{ print $1 "\t" $2 }' "$HOME/.lists/g"
 end
 
+complete -c g -l edit -l add
 complete -c g --no-files -a "(__g_keys)"
-
