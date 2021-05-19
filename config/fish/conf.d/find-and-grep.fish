3c664862b658c8cdd71288a5c708f67a4b49f0b8
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-05-19 10:11:47 -0700

    Refactor ff with argparse #fish

--- config/fish/conf.d/find-and-grep.fish
+++ config/fish/conf.d/find-and-grep.fish
@@ -4,21 +4,21 @@ alias rg_ "rg --no-heading --no-filename --no-line-number"
 
 function ff
   set -l flags
-  if test -n "$argv"
-      and test $argv[1] = "-u" # -u belongs to first rg
-      set flags $argv[1]
-      set -e argv[1]
+
+  argparse u/unrestricted -- $argv
+  or return
+
+  # --unrestricted
+  if set -q _flag_unrestricted
+      set flags "-u"
   end
 
-  set -l pattern "^"
-  if test -n "$argv"
-    set pattern $argv
+  if test -z "$argv"
+    rg --files --follow $flags
+  else
+    rg --files --follow $flags | rg $argv
   end
-
-  rg --files --follow $flags | rg $pattern
 end
-
-alias ffu "ff -u"
 
 function fne
   ff "\\.$argv[1]\$"
