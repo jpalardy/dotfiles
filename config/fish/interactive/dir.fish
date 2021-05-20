9d8a9dba67a678946d603fec715260fd836e8f77
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-05-20 11:51:32 -0700

    Combine tad/dnd into dir.fish #fish

--- config/fish/interactive/dir.fish
+++ config/fish/interactive/dir.fish
@@ -12,3 +12,13 @@ function tad
   rm -rf $dst
   cd $start_cd
 end
+
+# date name directory
+function dnd
+  if test (count $argv) = 0
+    echo >&2 "missing directory name"
+    return 1
+  end
+  set -l name (date +%F)-(string join "-" $argv)
+  mkdir $name; and cd $name
+end
--- config/fish/interactive/dnd.fish
+++
@@ -1,9 +0,0 @@
-# date name directory
-function dnd
-  if test (count $argv) = 0
-    echo >&2 "missing directory name"
-    return 1
-  end
-  set -l name (date +%F)-(string join "-" $argv)
-  mkdir $name; and cd $name
-end
