e033f089adad6fe29844dce2d401dd5bd9180b63
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-06-20 11:21:03 -0700

    Allow gcp on remote-less repos #fish

--- config/fish/interactive/git.fish
+++ config/fish/interactive/git.fish
@@ -19,7 +19,14 @@ function gcp
   git config alias.gcp.enabled 2>/dev/null | read -l gcp_config
   if test "$gcp_config" != "true"
     echo >&2 "gcp is disabled for this repository -- fix: git config alias.gcp.enabled true"
-    return
+    return 1
   end
-  git commit -av -m '-'; and git push
+  git commit -av -m '-'; or return 1
+  git remote | read -l remotes
+  if test -n "$remotes"
+    git push
+  else
+    echo >&2 "no remotes"
+    return 1
+  end
 end
