708972b1559faf2344797a1560ce956d9724ff21
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-07-22 10:05:28 -0700

    Revert glg to abbr w/ --all #fish

--- config/fish/interactive/git.fish
+++ config/fish/interactive/git.fish
@@ -4,16 +4,7 @@ function git_diff
   git diff $argv &| vim -R -
 end
 
-function glg
-  set -l heads HEAD
-  if git rev-parse @{upstream} &>/dev/null
-    set heads $heads @{upstream}
-  end
-  git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset%C(auto)%d%Creset %C(dim white)%an%Creset' $heads $argv
-end
-
-complete -c glg -f
-complete -c glg -l all
+abbr --add --global glg "git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr)%Creset%C(auto)%d%Creset %C(dim white)%an%Creset' --all"
 
 function gcp
   git config alias.gcp.enabled 2>/dev/null | read -l gcp_config
