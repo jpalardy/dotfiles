a27becd34fdf1b5babd764c6fd615ba9d25d73e7
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-08-23 09:31:04 -0700

    Add postgres snippets #chunks

--- chunks/chunks.sql
+++ chunks/chunks.sql
@@ -11,6 +11,26 @@ ORDER BY col DESC;
 WHERE random() < 0.05
 LIMIT 30
 
+-- truncate, with sequence reset
+
+TRUNCATE TABLE tbl_name RESTART IDENTITY CASCADE;
+
+-- count all tables
+
+SELECT schemaname, relname, n_live_tup
+FROM pg_stat_user_tables
+ORDER BY schemaname, relname;
+
+-- search_path management
+
+\dn       -- list schemas
+
+SHOW search_path;
+
+SET search_path TO other_schema, public;
+
+-- -------------------------------------------------
+
 -- .pgpass @ 600
 -- hostname:port:database:username:password
 
