b3e4bc2a7916c272589991498f51ba890c6415cb
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-09-10 10:23:44 -0700

    Use --group-directories-first only if available

--- config/fish/interactive/ll.fish
+++ config/fish/interactive/ll.fish
@@ -1,3 +1,6 @@
-function ll --wraps ls --description "List contents of directory using long format"
+# detect if flag supported
+if ls --group-directories-first &> /dev/null
+  function ll --wraps ls --description "List contents of directory using long format"
     ls -lh --group-directories-first $argv
+  end
 end
