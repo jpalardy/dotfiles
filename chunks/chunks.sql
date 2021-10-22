da3011bd40e7a87ba6961798eb12b6c0e7c69df4
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-10-21 17:05:14 -0700

    Move part of SQL chunk to memora

--- chunks/chunks.sql
+++ chunks/chunks.sql
@@ -1,11 +1,4 @@
 
--- tabulate a column
-
-SELECT   col, COUNT(*)
-FROM
-GROUP BY col
-ORDER BY col DESC;
-
 -- get a sample
 
 WHERE random() < 0.05
@@ -20,14 +13,6 @@ TRUNCATE TABLE tbl_name RESTART IDENTITY CASCADE;
 SELECT schemaname, relname, n_live_tup
 FROM pg_stat_user_tables
 ORDER BY schemaname, relname;
-
--- search_path management
-
-\dn       -- list schemas
-
-SHOW search_path;
-
-SET search_path TO other_schema, public;
 
 -- -------------------------------------------------
 
