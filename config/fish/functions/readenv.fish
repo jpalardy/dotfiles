10392fe09dfb85896eacea180e8f85f6aaebf4c4
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-01-02 11:22:01 -0800

    Fix readenv (updated syntax)... #fish

--- config/fish/functions/readenv.fish
+++ config/fish/functions/readenv.fish
@@ -1,4 +1,4 @@
 function readenv
   set -l filename $argv[1] .env
-  awk 'match($0, /^(export ?)(.+)( +|= *)(.*)/, arr) { print "set -gx", arr[2], arr[4]}' $filename[1]
+  awk -F '=' '{print "set -gx", $1, "\"" $2 "\""}' $filename[1]
 end
