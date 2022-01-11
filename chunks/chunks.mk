b5ab35fe03dc6169e6586286aa7a01db2219b56e
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-01-11 14:13:04 -0800

    Add INTERMEDIATE example #chunks

--- chunks/chunks.mk
+++ chunks/chunks.mk
@@ -36,6 +36,16 @@ clean:
 	rm -f $(obj) myprog
 
 #-------------------------------------------------
+# making some targets as intermediate, for cleanup
+#-------------------------------------------------
+
+# details https://www.gnu.org/software/make/manual/html_node/Chained-Rules.html
+
+.INTERMEDIATE: whatever
+.PRECIOUS: whatever
+# .SECONDARY: whatever
+
+#-------------------------------------------------
 # generated auto-include
 #-------------------------------------------------
 
