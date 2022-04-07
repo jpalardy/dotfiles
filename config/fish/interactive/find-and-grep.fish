7cb1bf13e382e58e18b3fd1995bbaba8697e371d
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-04-07 08:49:24 -0700

    Update ff, add ftf #fish

--- config/fish/interactive/find-and-grep.fish
+++ config/fish/interactive/find-and-grep.fish
@@ -1,26 +1,11 @@
 alias rg "rg -i --sort-files"
 alias rg_ "rg --no-heading --no-filename --no-line-number"
 
-function ff
-  set -l flags
-
-  argparse u/unrestricted -- $argv
-  or return
-
-  # --unrestricted
-  if set -q _flag_unrestricted
-      set flags "-u"
-  end
-
-  if test -z "$argv"
-    rg --files --follow $flags
-  else
-    rg --files --follow $flags | rg $argv
-  end
-end
+alias ff "rg --files --follow"
+alias ftf "find . -type f"
 
 function fne
-  ff "\\.$argv[1]\$"
+  ff | rg "\\.$argv[1]\$"
 end
 
 function f.
