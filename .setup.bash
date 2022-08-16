9b6b647ddee073b5ddc8f44475427d29cec19761
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-08-15 20:34:25 -0700

    Consolidate .setup* scripts #shell

--- .setup.bash
+++ .setup.bash
@@ -1,26 +1,30 @@
 #!/bin/bash
 
+DOTFILES=$(pwd)
 first=1
 
 relink() {
-  if [ "$(readlink "$HOME/$1")" = "$2" ]; then
+  if [ "$(readlink "$BASE_DIR/$1")" = "$2" ]; then
     return
   fi
   if [[ $first = 1 ]]; then
     echo "set -e"
     first=0
   fi
-  if [[ -e "$HOME/$1" || -L "$HOME/$1" ]]; then
-    echo "rm -f $HOME/$1"
+  if [[ -e "$BASE_DIR/$1" || -L "$BASE_DIR/$1" ]]; then
+    echo "rm -f $BASE_DIR/$1"
   fi
-  echo "ln -s -v $2 $HOME/$1"
+  echo "ln -s -v $2 $BASE_DIR/$1"
 }
 
-DOTFILES=$(pwd)
+BASE_DIR="$HOME"
 
-relink .bashrc       "$DOTFILES/bash/bashrc"
-relink .bash_profile "$DOTFILES/bash/bash_profile"
-relink .bash_logout  "$DOTFILES/bash/bash_logout"
+if [ -n "$NEED_BASH" ]; then
+  relink .bashrc       "$DOTFILES/bash/bashrc"
+  relink .bash_profile "$DOTFILES/bash/bash_profile"
+  relink .bash_logout  "$DOTFILES/bash/bash_logout"
+fi
+
 relink .inputrc      "$DOTFILES/bash/inputrc"
 
 relink .vim          "$DOTFILES/vim"
@@ -46,4 +50,11 @@ relink .digrc        "$DOTFILES/digrc"
 relink .psqlrc       "$DOTFILES/psqlrc"
 relink .sqliterc     "$DOTFILES/sqliterc"
 relink .slate        "$DOTFILES/slate"
+
+# -------------------------------------------------
+
+BASE_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+relink fish     "$DOTFILES/config/fish"
+relink yamllint "$DOTFILES/config/yamllint"
 
--- Makefile
+++ Makefile
@@ -1,13 +1,7 @@
 
 preview:
 	@bash .setup.bash
-	@bash .setup-xdg.bash
-	@diff -q ssh/config ~/.ssh/config || exit 0
 
-run:
-	@bash .setup.bash | bash
-	@bash .setup-xdg.bash | bash
-
-ssh-diff:
+diff:
 	@vimdiff ssh/config ~/.ssh/config
 
