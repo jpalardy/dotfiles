26dca168d24f92634b1e93a509f2f905f4ba7c37
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-08-17 13:56:20 -0700

    Add `gcp --enable` instead of copy-paste #fish

--- config/fish/interactive/git.fish
+++ config/fish/interactive/git.fish
@@ -6,9 +6,18 @@ end
 abbr --add --global glg "git log --graph --pretty=colorful --all"
 
 function gcp
+  argparse enable -- $argv
+  or return
+
+  # --enable
+  if set -q _flag_enable
+    git config alias.gcp.enabled true
+    return
+  end
+
   git config alias.gcp.enabled 2>/dev/null | read -l gcp_config
   if test "$gcp_config" != "true"
-    echo >&2 "gcp is disabled for this repository -- fix: git config alias.gcp.enabled true"
+    echo >&2 "gcp is disabled for this repository: gcp --enable?"
     return 1
   end
   git commit -av -m '-'; or return 1
