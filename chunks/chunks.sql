2b92c6b08661a4be8efee7f45037240419b8d7ba
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-11-22 15:21:46 -0800

    Add pg_stat_activity query #chunks

--- chunks/chunks.sql
+++ chunks/chunks.sql
@@ -21,6 +21,13 @@ FROM information_schema.triggers
 GROUP BY 1, 2
 ORDER BY 1, 2;
 
+-- db activity
+
+SELECT pid, age(clock_timestamp(), query_start), usename, query
+FROM pg_stat_activity
+WHERE query != '<IDLE>' AND query NOT ILIKE '%pg_stat_activity%'
+ORDER BY query_start desc;
+
 -- -------------------------------------------------
 
 -- export to CSV
