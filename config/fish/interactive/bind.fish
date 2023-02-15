804a764324ea01dc714e271f2b7710186be8796d
Jonathan Palardy <jonathan.palardy@gmail.com>
2023-02-14 16:49:05 -0800

    Use updated command substitution syntax for interactive uses #fish

--- config/fish/interactive/bind.fish
+++ config/fish/interactive/bind.fish
@@ -2,13 +2,13 @@
 bind \eh 'commandline -r "vim -c \"normal h\""; commandline -f execute'
 
 # esc-`
-bind \e` 'commandline -i "($history[1])"'
+bind \e` 'commandline -i "\$($history[1])"'
 
 # esc-w
 bind \ew fzf-warp
 
 # ctrl-_ -- open rg results in vim
-bind \c_ 'commandline -r "vim -q ($history[1] --vimgrep | psub)"; commandline -f execute'
+bind \c_ 'commandline -r "vim -q \$($history[1] --vimgrep | psub)"; commandline -f execute'
 
 # esc-,
 bind \e, fzf-vimrepl
