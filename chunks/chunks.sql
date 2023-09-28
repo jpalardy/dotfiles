3b8b3114d78580dd045b4d9071834cc5cf194c38
Jonathan Palardy <jonathan.palardy@gmail.com>
2023-09-28 09:13:21 -0700

    Add SQL chunk

--- chunks/chunks.sql
+++ chunks/chunks.sql
@@ -21,12 +21,18 @@ FROM information_schema.triggers
 GROUP BY 1, 2
 ORDER BY 1, 2;
 
--- db activity
+-- db activity: current queries
 
 SELECT pid, age(clock_timestamp(), query_start), usename, query
 FROM pg_stat_activity
 WHERE query != '<IDLE>' AND query NOT ILIKE '%pg_stat_activity%'
 ORDER BY query_start desc;
+
+-- db activity: avg time per query
+
+SELECT LEFT (query, 100), calls AS number_of_calls, (total_time / calls)::integer AS avg_time_ms
+FROM pg_stat_statements
+ORDER BY avg_time_ms DESC;
 
 -- temporary table; for export
 DROP TABLE IF EXISTS results;
