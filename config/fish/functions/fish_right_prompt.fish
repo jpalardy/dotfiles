ffd1225ad5fa20548df1835ce0a802e4db90e994
Jonathan Palardy <jonathan.palardy@gmail.com>
2023-11-22 13:23:14 -0500

    Show git upstream info in prompt #fish

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,4 @@
 function fish_right_prompt
   set -l normal (set_color normal)
-  set -q fish_prompt_pwd_dir_length; or set -lx fish_prompt_pwd_dir_length 0
-
-  echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_git_prompt) $normal
+  echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_git_prompt)
 end
--- config/fish/login/vars.fish
+++ config/fish/login/vars.fish
@@ -26,6 +26,17 @@ set -x CLICOLOR 1    # used by `tree`, possibly others
 set -x MANPAGER "manpager"
 
 #-------------------------------------------------
+# PROMPT
+#-------------------------------------------------
+
+set -g fish_prompt_pwd_dir_length 0
+
+set -g __fish_git_prompt_showcolorhints true
+set -g __fish_git_prompt_showupstream verbose
+set -g __fish_git_prompt_char_upstream_ahead '+'
+set -g __fish_git_prompt_char_upstream_behind '-'
+
+#-------------------------------------------------
 # PATHs
 #-------------------------------------------------
 
