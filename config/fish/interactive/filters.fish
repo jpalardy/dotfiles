8872a57f0aec03566620bfc36cc8a76a3d6f3602
Jonathan Palardy <jonathan.palardy@gmail.com>
2023-11-28 08:53:57 -0500

    Add _diff filter #fish

--- config/fish/interactive/filters.fish
+++ config/fish/interactive/filters.fish
@@ -16,6 +16,10 @@ function __sum
   awk '{sum+=$1} END {print sum}' $argv
 end
 
+function __diff
+  awk 'prev { printf "%3d\n", $1-prev } { prev=$1 }' $argv
+end
+
 function __uniq
   awk '!seen[$0]++' $argv
 end
