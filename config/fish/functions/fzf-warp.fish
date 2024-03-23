e02cf55799d63b64404112c33b39e74d26305dca
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-03-23 00:26:57 -0700

    Put fzf-warp with its binding #fish

--- config/fish/functions/fzf-warp.fish
+++
@@ -1,8 +0,0 @@
-function fzf-warp
-  set -l config "$HOME/.lists/warp"
-
-  fzf < $config | read -l result
-  and commandline -- "ssh $result"
-
-  commandline -f repaint
-end
--- config/fish/interactive/bind.fish
+++ config/fish/interactive/bind.fish
@@ -1,11 +1,21 @@
+
+function fzf-warp
+  set -l config "$HOME/.lists/warp"
+  fzf < $config | read -l result
+  and commandline -- "ssh $result"
+  commandline -f repaint
+end
+
+# esc-w
+bind \ew fzf-warp
+
+# -------------------------------------------------
+
 # esc-h
 bind \eh 'commandline -r "vim -c \"normal h\""; commandline -f execute'
 
 # esc-`
 bind \e` 'commandline -i "\$($history[1])"'
-
-# esc-w
-bind \ew fzf-warp
 
 # ctrl-_ -- open rg results in vim
 bind \c_ 'commandline -r "vim -q \$($history[1] --vimgrep | psub)"; commandline -f execute'
