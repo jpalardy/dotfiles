833cdcc6c1edfe6958206438155bc96c3c7e226c
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-05-23 21:02:25 -0700

    Make ff really dumb #fish

--- config/fish/interactive/find-and-grep.fish
+++ config/fish/interactive/find-and-grep.fish
@@ -1,20 +1,4 @@
 alias rg "rg -i --sort path"
 alias rg_ "rg --no-heading --no-filename --no-line-number"
 
-function ff
-  set -l restricted
-  if string match -q -r '^-u' -- $argv[1]
-    set restricted $argv[1]
-    set --erase argv[1]
-  end
-  if test (count $argv) -gt 0
-    rg --files --follow $restricted | rgm $argv
-  else
-    rg --files --follow $restricted
-  end
-end
-
-function fz
-  rg_ $argv | fzf --exit-0
-end
-
+alias ff 'rg --files --follow --color=never'
