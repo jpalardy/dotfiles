ab7b5182db9cb19d866b8860f85d35b92d0edd95
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-07-31 09:53:28 -0400

    Move helper function to its single call site #fish

--- config/fish/functions/__fish_paginate.fish
+++ config/fish/functions/__fish_paginate.fish
@@ -1,3 +1,13 @@
+function commandline-append
+  if test -z (commandline -j | string join '')
+    commandline -i $history[1]
+  end
+
+  set -l trimmed_cmd (commandline -b | string trim)
+  commandline -r "$trimmed_cmd $argv"
+  commandline -f repaint
+end
+
 function __fish_paginate
   commandline-append "&| vim -R -"
 end
--- config/fish/functions/commandline-append.fish
+++
@@ -1,9 +0,0 @@
-function commandline-append
-  if test -z (commandline -j | string join '')
-    commandline -i $history[1]
-  end
-
-  set -l trimmed_cmd (commandline -b | string trim)
-  commandline -r "$trimmed_cmd $argv"
-  commandline -f repaint
-end
