7b8bf4802c99d290da05e3cfb37fc5b1510dd197
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-08-09 09:07:03 -0700

    Update gl
    - lists are plural
    - unused glss

--- config/fish/interactive/gl.fish
+++ config/fish/interactive/gl.fish
@@ -2,10 +2,8 @@ function gl
   set -l path "$HOME/Downloads"
   set -q argv[1]; and set path (string replace -r '/$' '' $argv[1])
 
-  ls -t $path | fzf --multi | while read -l r; set result $result $r; end
-  for r in $result
+  ls -t $path | fzf --multi | while read -l r; set results $results $r; end
+  for r in $results
     mv -v "$path/$r" .
   end
 end
-
-alias glss "gl $HOME/Desktop/screenshots"
