2365417e3920748c466f46bc0cceb3996b4e43d6
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-09-05 15:21:27 -0700

    Clean up SQL chunks

--- chunks/chunks.sql
+++ chunks/chunks.sql
@@ -1,23 +1,15 @@
 
--- get a sample
-
-WHERE random() < 0.05
-LIMIT 30
-
 -- truncate, with sequence reset
-
-TRUNCATE TABLE tbl_name RESTART IDENTITY CASCADE;
+TRUNCATE TABLE "tbl_name" RESTART IDENTITY CASCADE;
 
 -- count all tables
-
 SELECT schemaname, relname, n_live_tup
-FROM pg_stat_user_tables
+FROM "pg_stat_user_tables"
 ORDER BY schemaname, relname;
 
 -- show triggers
-
 SELECT event_object_table AS table_name, trigger_name, array_agg(event_manipulation) AS events
-FROM information_schema.triggers
+FROM "information_schema.triggers"
 GROUP BY 1, 2
 ORDER BY 1, 2;
 
@@ -31,7 +23,7 @@ SELECT pg_stat_statements_reset();
 
 -- db activity: current queries
 SELECT pid, age(clock_timestamp(), query_start), usename, query
-FROM pg_stat_activity
+FROM "pg_stat_activity"
 WHERE query != '<IDLE>'
   AND query NOT ILIKE '%pg_stat_activity%'
 ORDER BY query_start DESC;
@@ -43,17 +35,17 @@ select pg_terminate_backend(pid);
 
 -- db activity: avg time per query
 SELECT LEFT (query, 100), calls, (total_time / calls)::integer AS avg_time_ms
-FROM pg_stat_statements
+FROM "pg_stat_statements"
 ORDER BY avg_time_ms DESC;
 
 -- db activity: index usage
 SELECT relname, indexrelname, idx_scan
-FROM pg_stat_user_indexes
+FROM "pg_stat_user_indexes"
 ORDER BY idx_scan;
 
 -- db activity: seq/idx scans
 SELECT schemaname, relname, seq_scan, seq_tup_read, idx_scan, seq_tup_read / seq_scan AS avg
-FROM pg_stat_user_tables
+FROM "pg_stat_user_tables"
 WHERE seq_scan > 0
 ORDER BY seq_tup_read DESC;
 
@@ -69,7 +61,6 @@ SELECT * FROM results;
 -- -------------------------------------------------
 
 -- export to CSV
-
 \copy (SELECT * FROM results) to 'results.csv' with csv
 \copy (SELECT * FROM results) to 'results.csv' with csv header
 
