e01b977c6642775edf536baec4262c48c2eba176
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-10-09 12:25:08 -0700

    Put tab config in wezterm.lua
    mostly stubs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -15,6 +15,12 @@ config.color_scheme = "Tokyo Night"
 
 -- tabs
 config.hide_tab_bar_if_only_one_tab = true
+-- config.show_close_tab_button_in_tabs = false
+-- config.show_new_tab_button_in_tab_bar = false
+-- config.show_tab_index_in_tab_bar = false
+-- config.show_tabs_in_tab_bar = false
+-- config.use_fancy_tab_bar = false
+config.enable_tab_bar = false
 
 -- windows
 config.initial_cols = 120
