732ee0143e3de206ead428e94f3daae0e491a041
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-10-17 09:02:24 -0700

    Allow "empty" ff search

--- config/fish/interactive/find-and-grep.fish
+++ config/fish/interactive/find-and-grep.fish
@@ -1,6 +1,6 @@
 alias rg "rg -i --sort path"
 alias rg_ "rg --no-heading --no-filename --no-line-number"
 
-abbr --add ff 'rg --files --follow | rgm'
-abbr --add --set-cursor ffu 'rg --files --follow -u% | rgm'
+abbr --add ff 'rg --files --follow | rgm ^'
+abbr --add --set-cursor ffu 'rg --files --follow -u% | rgm ^'
 
