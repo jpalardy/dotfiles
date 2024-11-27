90c3dcd68e59b5393053e3a323f45ec0cc8da9d2
Jonathan Palardy <jonathan.palardy@gmail.com>
2024-11-27 15:54:17 -0800

    Simplify __fish_paginate

--- config/fish/functions/__fish_paginate.fish
+++ config/fish/functions/__fish_paginate.fish
@@ -1,13 +1,3 @@
-function commandline-append
-  if test -z (commandline -j | string join '')
-    commandline -i $history[1]
-  end
-
-  set -l trimmed_cmd (commandline -b | string trim)
-  commandline -r "$trimmed_cmd $argv"
-  commandline -f repaint
-end
-
 function __fish_paginate
-  commandline-append "&| vim -R -"
+  fish_commandline_append " &| vim -R -"
 end
