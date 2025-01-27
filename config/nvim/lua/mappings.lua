5e9d65c53385662fe38b67a6141e3d8fe6f51e84
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-01-27 09:02:38 -0800

    Add blank-trimmer mapping

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -42,6 +42,20 @@ vim.keymap.set("i", "<F7>", "<C-o>:set wrap!<CR>")
 vim.keymap.set({ "n", "x" }, ",v", ":v/<C-r>//d<CR>gg", { silent = true })
 vim.keymap.set({ "n", "x" }, ",d", ":g/<C-r>//d<CR>gg", { silent = true })
 
+-- translation of vimscript logic, might need tweaking
+local function preserve(command)
+  local saved_search = vim.fn.getreg("/")
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  vim.cmd(command)
+  vim.api.nvim_win_set_cursor(0, cursor)
+  vim.fn.setreg("/", saved_search)
+end
+
+-- empty line trim
+vim.keymap.set("n", "_B", function()
+  preserve("g/\\_^\\n\\{2,}/d")
+end)
+
 -- cycle between colorschemes
 vim.keymap.set("n", ",c", function()
   local schemes = { "gruvbox", "gruvbox-material", "tokyonight-storm", "tokyonight-moon" }
