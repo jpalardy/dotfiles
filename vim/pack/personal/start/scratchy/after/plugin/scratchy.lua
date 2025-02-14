9ce992f806d5dbb1aeed9e33465fcf741b19fa5b
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-02-14 08:55:56 -0800

    Put scratchy files into quickfix list w/ <c-q>

---
+++ vim/pack/personal/start/scratchy/after/plugin/scratchy.lua
@@ -0,0 +1,20 @@
+local send_all_to_qf = function()
+  local lines = vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), { type = "V" })
+
+  local qf_entries = {}
+  for i, line in ipairs(lines) do
+    table.insert(qf_entries, { filename = line, lnum = 1, col = 1 })
+  end
+
+  vim.fn.setqflist(qf_entries, "r")
+  vim.cmd([[cfirst]])
+  vim.cmd([[botright copen]])
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "scratchy",
+  callback = function()
+    vim.keymap.set("v", "<c-q>", send_all_to_qf)
+    vim.keymap.set("v", "<CR>", send_all_to_qf)
+  end,
+})
