8ad608edc2770c94546d6e961600c4bbe9d6480a
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-06-04 10:46:02 -0700

    Add some R chunks

--- chunks/chunks.R
+++ chunks/chunks.R
@@ -1,8 +1,28 @@
 library(tidyverse)
 theme_set(theme_bw())
 
+# -------------------------------------------------
 # scatter
+# -------------------------------------------------
+
 d |>
   ggplot(aes(x, y)) +
   geom_point(alpha = 0.7, size = 3, stroke = 0, color = "#0367a6") +
   NULL
+
+# -------------------------------------------------
+# density
+# -------------------------------------------------
+
+d |>
+  ggplot(aes(x)) +
+  geom_density(alpha = 0.5, fill = "#0367a6") +
+  NULL
+
+# -------------------------------------------------
+# compositing graphs
+# -------------------------------------------------
+
+library(gridExtra)
+
+list(p1, p2) |> grid.arrange(grobs = _, ncol = 1)
