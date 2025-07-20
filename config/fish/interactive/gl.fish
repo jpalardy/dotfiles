87d49ea6283a64f6ec11a7d7269bc6939e7a6c67
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-07-20 09:52:23 -0700

    Embed logic directly in while-loop

--- config/fish/interactive/gl.fish
+++ config/fish/interactive/gl.fish
@@ -2,8 +2,7 @@ function gl
   set -l path "$HOME/Downloads"
   set -q argv[1]; and set path (string replace -r '/$' '' $argv[1])
 
-  ls -t $path | fzf --multi | while read -l r; set results $results $r; end
-  for r in $results
+  ls -t $path | fzf --multi | while read -l r
     mv -v "$path/$r" .
   end
 end
