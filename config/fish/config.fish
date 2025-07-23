5249e4ae81aa6a556d0e6a366ab082d9e9e71a55
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-07-23 10:05:06 -0700

    Clean up spacing and comments

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,26 +1,14 @@
-#-------------------------------------------------
-# LOGIN
-#-------------------------------------------------
-
 if status --is-login
   for file in (status dirname)/login/*.fish
     source $file
   end
 end
 
-#-------------------------------------------------
-# INTERACTIVE
-#-------------------------------------------------
-
 if status --is-interactive
   for file in (status dirname)/interactive/*.fish
     source $file
   end
 end
-
-#-------------------------------------------------
-# LOCAL
-#-------------------------------------------------
 
 for file in (status dirname)/local/*.fish
   source $file
