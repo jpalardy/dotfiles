6167334088d1bee5b231cb96f5fd1a7a34870d8d
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-09-12 12:58:15 -0700

    Add experimental vim binding for R

--- config/nvim/ftplugin/r.lua
+++ config/nvim/ftplugin/r.lua
@@ -7,3 +7,7 @@ end)
 vim.keymap.set("n", "<c-c>?", function()
   vim.cmd(string.format("SlimeSend1 head(%s, n=25)", vim.fn.expand("<cword>")))
 end)
+
+vim.keymap.set("n", "<c-c>g", function()
+  vim.cmd(string.format("SlimeSend1 dplyr::glimpse(%s)", vim.fn.expand("<cword>")))
+end)
