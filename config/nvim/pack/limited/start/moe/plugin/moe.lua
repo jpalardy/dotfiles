2715ca8821dfb2180d2913743b74a3e57cb12363
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-01-20 11:14:06 -0800

    Fold until :done, for .moe file

--- config/nvim/pack/limited/start/moe/plugin/moe.lua
+++ config/nvim/pack/limited/start/moe/plugin/moe.lua
@@ -1,6 +1,27 @@
+local function fold_until_done()
+  -- Get all lines in the buffer
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+  -- Find the last line containing `:fold`
+  local fold_line = nil
+  for i = #lines, 1, -1 do
+    if lines[i]:match(":done") then
+      fold_line = i
+      break
+    end
+  end
+
+  -- If we found a :fold line, create the fold
+  if fold_line and fold_line > 1 then
+    -- Create a fold from line 1 to the fold_line
+    vim.cmd(string.format("1,%dfold", fold_line))
+  end
+end
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.moe" },
   callback = function()
     vim.bo.filetype = "moe"
+    fold_until_done()
   end,
 })
