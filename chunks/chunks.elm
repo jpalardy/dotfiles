71a1136fb6d335e36eb83508f5269863f6d93b71
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-04-25 11:22:42 -0700

    Add more convenient elm chunks

--- chunks/chunks.elm
+++ chunks/chunks.elm
@@ -2,3 +2,18 @@
 renderMaybe : Maybe a -> (a -> Html msg) -> Html msg
 renderMaybe value f =
   Maybe.map f value |> Maybe.withDefault (Html.text "")
+
+-------------------------------------------------
+
+renderIf : Bool -> Html msg -> Html msg
+renderIf condition content =
+    ifelse condition content (Html.text "")
+
+ifelse : Bool -> a -> a -> a
+ifelse condition v1 v2 =
+    if condition then
+        v1
+
+    else
+        v2
+
