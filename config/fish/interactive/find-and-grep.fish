3cc2d0a90caf888d63818dd3f8796c3f313d7099
Jonathan Palardy <jonathan.palardy@gmail.com>
2026-07-15 09:46:07 -0700

    fish: add find-modtime

--- config/fish/interactive/find-and-grep.fish
+++ config/fish/interactive/find-and-grep.fish
@@ -4,3 +4,7 @@ alias rg_ "rg --no-heading --no-filename --no-line-number"
 abbr --add ff 'rg --files --follow | rgm ^'
 abbr --add --set-cursor ffu 'rg --files --follow -u% | rgm ^'
 
+function find-modtime
+  rg --files -0 | find -files0-from - -printf '%T@\t%p\n' | sort -n | awk -F'\t' '{print strftime("%Y-%m-%dT%H:%M:%S", $1), $2}'
+end
+
